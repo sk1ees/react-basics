96f950ed12757e220d21298416f2ea76a2552a40
Deep Sarkar <144143057+sk1ees@users.noreply.github.com>
2024-07-14 20:54:03 +0530

    added navbar

--- restaurant-basics/src/components/basics/Restaurant.js
+++ restaurant-basics/src/components/basics/Restaurant.js
@@ -4,8 +4,46 @@ import Menu from "./menuAPI";
 import MenuCard from "./MenuCard";
 const Restaurant = () => {
   const [menuData, setMenuData] = useState(Menu);
+
+  const filterItem = (category) => {
+    const updatedList = Menu.filter((item) => {
+      return item.category === category;
+    });
+    setMenuData(updatedList);
+  };
   return (
     <>
+      <nav className="navbar">
+        <div className="btn-group">
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("breakfast")}
+          >
+            BreakFast
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("lunch")}
+          >
+            Lunch
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("evening")}
+          >
+            Evening
+          </button>
+          <button
+            className="btn-group__item"
+            onClick={() => filterItem("dinner")}
+          >
+            Dinner
+          </button>
+          <button className="btn-group__item" onClick={() => setMenuData(Menu)}>
+            All
+          </button>
+        </div>
+      </nav>
       <MenuCard menuData={menuData} />
     </>
   );
