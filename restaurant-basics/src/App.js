78e01f7200a27d58d864ad45b1758553fd0ff02f
Deep Sarkar <144143057+sk1ees@users.noreply.github.com>
2024-07-17 09:30:22 +0530

    worked on userReducer-hook

--- restaurant-basics/src/App.js
+++ restaurant-basics/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 // import Restaurant from "./components/basics/Restaurant";
 // import UseState from "./components/Hooks/useState";
-import UseEffect from "./components/Hooks/useEffect";
+// import UseEffect from "./components/Hooks/useEffect";
+import UseReducer from "./components/Hooks/useReducer";
 
 const App = () => {
   return (
     <>
-      <UseEffect />
+      <UseReducer />
     </>
   );
 };
