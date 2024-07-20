69a0e6d534ae0fe3c81f010d0c2c564925a0a4a6
Deep Sarkar <144143057+sk1ees@users.noreply.github.com>
2024-07-20 13:31:42 +0530

    workedon-react-router-dom

--- projects/bootstrap-textUtils/src/App.jsx
+++ projects/bootstrap-textUtils/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Alert from './components/Alert'
-// import About from './components/About'
+import About from './components/About'
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 import React, { useState } from 'react'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 function App() {
   const [mode, setMode] = useState("light")
   const [alert, setAlert] = useState(null)
@@ -39,11 +44,18 @@ function App() {
 
   return (
     <>
-
-      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <TextForm showAlert={showAlert} heading="Enter the text to utilize" mode={mode} toggleMode={toggleMode} />
-      {/* <About /> */}
+      <Router>
+        <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below:-" mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+        {/* <TextForm showAlert={showAlert} heading="Enter the text to utilize" mode={mode} toggleMode={toggleMode} /> */}
+        {/* <About /> */}
+      </Router>
     </>
   )
 }
