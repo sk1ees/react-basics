68c3b24bb9356bd58ff363a0e66dd83f1ab21b9f
sk1ees <deepsarkar18877@gmail.com>
2024-07-23 20:42:32 +0530

    worked on JWT & bycrypt

--- projects/iNoteBook/inotebook/backend/index.js
+++ projects/iNoteBook/inotebook/backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 connectToMongo(); 
 
 const app = express()
-const port = 6969
+const port = 5000
 app.use(express.json())
 app.use('/api/auth' , require('./routes/auth'))
 app.use('/api/notes' , require('./routes/notes'))
