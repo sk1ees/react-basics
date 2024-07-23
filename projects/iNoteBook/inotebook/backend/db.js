2da8c68b3b7c202185c6f9ee001523023de83fc9
sk1ees <deepsarkar18877@gmail.com>
2024-07-23 12:25:11 +0530

    added express

---
+++ projects/iNoteBook/inotebook/backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const mongoURI = "mongodb://localhost:27017/"
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+    .then(() => { console.log("Connected!") })
+    .catch((e) => { console.log(e.messsage) })
+}
+
+module.exports = connectToMongo;
