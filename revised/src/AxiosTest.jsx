6bb32478051972192c0063f3e7f9c7222d00a28d
sk1es <144143057+sk1ees@users.noreply.github.com>
2024-10-17 23:07:28 +0530

    context-api,axios,etc

--- revised/src/AxiosTest.jsx
+++ revised/src/AxiosTest.jsx
@@ -4,10 +4,12 @@ import { DataContext } from "./context/UserContext";
 
 const AxiosTest = () => {
   const userData = useContext(DataContext);
+  const [buttonText, setButtonText] = useState("Fetch the data!");
   const [data, setData] = useState([]);
   const getAxiosData = async () => {
     const response = await axios.get("https://picsum.photos/v2/list");
     setData(response.data);
+    setButtonText("Data Scraped Succesfully!");
     console.log(data);
   };
 
@@ -18,19 +20,26 @@ const AxiosTest = () => {
           <h1 className="text-5xl text-white p-3 text-center">
             Welcome {userData.userName}!
           </h1>
-          <button
-            onClick={getAxiosData}
-            className="px-5 py-2 bg-blue-500  rounded-lg active:scale-95"
-          >
-            Get the data
-          </button>
-          {data.map((item, id) => {
-            return (
-              <div key={id} className="w-full p-5 bg-zinc-800 mt-12 rounded-lg">
-                <img src={item.download_url} alt="" />
-              </div>
-            );
-          })}
+          <div className="w-full text-center py-5 ">
+            <button
+              onClick={getAxiosData}
+              className="px-5 py-2 bg-blue-500 text-center hover:bg-red-500 transition-all  rounded-lg active:scale-95"
+            >
+              {buttonText}
+            </button>
+          </div>
+          <div className="grid grid-cols-2 gap-5 p-5 bg-zinc-800 mt-12 rounded-lg">
+            {data.map((item, id) => {
+              return (
+                <img
+                  key={id}
+                  src={item.download_url}
+                  alt=""
+                  className="w-full"
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
